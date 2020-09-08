915abc3714afd79e1f0c362cfb85a698d70358e0
Yoon Su Hyeon <96-05-03@naver.com>
2020-09-09 00:50:45 +0900

    SMRP2 INIT

---
+++ app/src/main/java/com/example/smrpv2/ui/start/StartActivity.java
@@ -0,0 +1,25 @@
+package com.example.smrpv2.ui.start;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.smrpv2.R;
+/**
+ * StartActivity   :  ViewPager
+ * **/
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+
+    }
+
+
+
+}
