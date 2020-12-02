679b44e0dc291b0edd65880b3645c9910e3d538f
YoonSuHyeon <96-05-03@naver.com>
2020-12-02 17:04:17 +0900

    모델 탑재

--- app/src/main/java/com/example/smrpv2/ui/medicine/medshot/KakaoOCRActivity.java
+++ app/src/main/java/com/example/smrpv2/ui/medicine/medshot/KakaoOCRActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Matrix;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Bundle;
@@ -14,17 +16,34 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.smrpv2.R;
+
+import com.example.smrpv2.ml.ShapeModelVer11;
 import com.example.smrpv2.model.common.KaKaoResult;
 import com.example.smrpv2.model.common.KakaoDto;
 import com.example.smrpv2.model.Message;
 import com.example.smrpv2.retrofit.RetrofitHelper;
 import com.example.smrpv2.retrofit.RetrofitService_Server;
 
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.support.common.FileUtil;
+import org.tensorflow.lite.support.common.TensorProcessor;
+import org.tensorflow.lite.support.common.ops.DequantizeOp;
+import org.tensorflow.lite.support.common.ops.NormalizeOp;
+import org.tensorflow.lite.support.image.ImageProcessor;
+import org.tensorflow.lite.support.image.TensorImage;
+import org.tensorflow.lite.support.image.ops.ResizeOp;
+import org.tensorflow.lite.support.label.TensorLabel;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import okhttp3.MediaType;
 import okhttp3.MultipartBody;
@@ -143,7 +162,7 @@ import retrofit2.Response;
                 //카카오 OCR
                 sendKakaoOcr(frontfile, backfile);
                 sendFile(frontfile, backfile);
-
+                useModel(targetBitmap_front);
             } catch (Exception err) {
                 err.printStackTrace();
             }
@@ -153,6 +172,113 @@ import retrofit2.Response;
 
         }
 
+
+        private  void useModel(Bitmap targetBitmap_front){
+            try {
+                ImageProcessor imageProcessor =
+                        new ImageProcessor.Builder()
+                                .add(new ResizeOp(224, 224, ResizeOp.ResizeMethod.BILINEAR))
+                                .build();
+
+                ShapeModelVer11 model = ShapeModelVer11.newInstance(this);
+
+
+                // Creates inputs for reference.
+
+                //Drawable drawable = getDrawable(R.drawable.image8);
+
+               // Bitmap bitmap = ((BitmapDrawable)drawable).getBitmap();
+
+                ByteBuffer buffer= ByteBuffer.allocate(targetBitmap_front.getByteCount()); //바이트 버퍼를 이미지 사이즈 만큼 선언
+
+                targetBitmap_front.copyPixelsToBuffer(buffer);//비트맵의 픽셀을 버퍼에 저장
+
+                //
+                TensorBuffer inputI = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.FLOAT32);
+
+
+
+
+
+
+
+                TensorImage tImage = new TensorImage(DataType.FLOAT32);
+
+
+                tImage.load(targetBitmap_front);
+                tImage = imageProcessor.process(tImage);
+
+
+
+
+
+
+                TensorProcessor probabilityProcessor =
+                        new TensorProcessor.Builder().add(new DequantizeOp(0, 1/255.0f)).build();
+
+
+
+
+                TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.FLOAT32);
+
+
+
+                inputFeature0.loadBuffer(tImage.getBuffer());
+                TensorBuffer dequantizedBuffer = probabilityProcessor.process(inputFeature0);
+
+
+
+                // Runs model inference and gets result.
+                ShapeModelVer11.Outputs outputs = model.process(dequantizedBuffer);
+                TensorBuffer probabilityBuffer =
+                        TensorBuffer.createFixedSize(new int[]{1, 20}, DataType.FLOAT32);
+                probabilityBuffer=outputs.getOutputFeature0AsTensorBuffer();
+
+                @NonNull float[] floatArray = probabilityBuffer.getFloatArray();
+
+                float temp =0;
+                int idx=0;
+                for (int i = 0; i < floatArray.length; i++) {
+                    if(floatArray[i]> temp){
+                        temp=floatArray[i];
+                        idx=i;
+                    }
+                }
+
+                Log.d("idx:", idx + "   " + temp);
+                final String ASSOCIATED_AXIS_LABELS = "label.txt";
+                List<String> associatedAxisLabels = null;
+
+                try {
+                    associatedAxisLabels = FileUtil.loadLabels(this, ASSOCIATED_AXIS_LABELS);
+                } catch (IOException e) {
+                    Log.e("tfliteSupport", "Error reading label file", e);
+                }
+                Log.d("gg:", associatedAxisLabels.get(idx) );
+                // Releases model resources if no longer used.
+
+
+                TensorProcessor probabilityProcessor1 =
+                        new TensorProcessor.Builder().add(new NormalizeOp(0, 20)).build();
+
+                if (null != associatedAxisLabels) {
+                    // Map of labels and their corresponding probability
+                    TensorLabel labels = new TensorLabel(associatedAxisLabels,
+                            probabilityProcessor1.process(probabilityBuffer));
+
+                    // Create a map to access the result based on label
+                    Map<String, Float> floatMap = labels.getMapWithFloatValue();
+
+                }
+
+
+
+
+                model.close();
+            } catch (IOException e) {
+                // TODO Handle the exception
+            }
+        }
         private void sendKakaoOcr(File frontfile, File backfile) {
             Log.d("TAG", "frontfile.getAbsolutePath(): "+frontfile.getAbsolutePath());
             ///storage/emulated/0/Android/data/com.example.smrpv2/files/picF.jpg
